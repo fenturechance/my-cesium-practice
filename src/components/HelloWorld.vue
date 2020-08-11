65008fa18c71fd92c5d36ce2b6dc3db7a8b7dfe4
Jerry <fenturechance@gmail.com>
2020-08-11 17:34:41 +0800

    fly to taiwan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,12 +8,27 @@
 const Cesium = require('cesium') //無法使用import方法
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 export default {
+  data() {
+    return {
+      viewer: {},
+      taiwanPosition: {
+        latitude: 23.6978,
+        longitude: 120.9605
+      }
+    }
+  },
   mounted() {
     this.initCesium()
+    this.flyToTaiwan()
   },
   methods: {
     initCesium() {
-      let viewer = new Cesium.Viewer("cesiumContainer");
+      this.viewer = new Cesium.Viewer("cesiumContainer");
+    },
+    flyToTaiwan() {
+      let { latitude, longitude } = this.taiwanPosition
+      let height = 1000000
+      this.viewer.camera.flyTo({ destination: new Cesium.Cartesian3.fromDegrees(longitude, latitude, height) })
     }
   }
 }
