a3be91c43abf14ee0637b1e48f41d759c01d21ef
Jerry <fenturechance@gmail.com>
2020-08-17 09:50:56 +0800

    add arrow to confirm x y z

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,7 +23,7 @@ export default {
     this.initCesium()
     this.addDragon()
     this.addImageToDragonCenter()
-    this.addDebugModel()
+    this.addXYZArrow()
   },
   methods: {
     initCesium() {
@@ -34,8 +34,8 @@ export default {
           url : '/my-dragon/tileset.json'
       })
       this.tileset = this.viewer.scene.primitives.add(myTileset);
-      let heading = 1.2
-      let pitch = 0
+      let heading = 0.5
+      let pitch = -0.5
       let range = 0
       this.viewer.zoomTo(this.tileset, new Cesium.HeadingPitchRange(heading, pitch, range));
     },
@@ -56,9 +56,80 @@ export default {
         })
       })
     },
-    addDebugModel() {
-      let myDebugMatrix = new Cesium.DebugModelMatrixPrimitive()
-      this.viewer.scene.primitives.add(myDebugMatrix)
+    addXYZArrow() {
+      this.tileset.readyPromise.then(() => {
+        let center = this.tileset.boundingSphere.center
+        let axisX = this.addOneArrow({ color: Cesium.Color.RED, position: center, id: 1 })
+        let axisY = this.addOneArrow({ color: Cesium.Color.GREEN, position: center, id: 2 })
+        let axisZ = this.addOneArrow({ color: Cesium.Color.BLUE, position: center, id: 3 })
+        axisX = this.rotatePrimitive('x', axisX)
+        axisY = this.rotatePrimitive('y', axisY)
+        this.viewer.scene.primitives.add(axisX)
+        this.viewer.scene.primitives.add(axisY)
+        this.viewer.scene.primitives.add(axisZ)
+      })
+    },
+    addOneArrow(option) {
+      const { color, position, id } = option
+      const cylinderOption = {
+        length: 3000,
+        topRadius: 3,
+        bottomRadius: 3,
+      }
+      const cylinder = new Cesium.CylinderGeometry(cylinderOption)
+      const line = Cesium.CylinderGeometry.createGeometry(cylinder)
+      const arrowOption = {
+        length: 50,
+        topRadius: 0,
+        bottomRadius: 12
+      }
+      const arrowCylinder = new Cesium.CylinderGeometry(arrowOption)
+      const arrow = Cesium.CylinderGeometry.createGeometry(arrowCylinder)
+      let offset = ( 3000 + 50 ) / 2
+      this.translateArrowPosition(arrow, [0, 0, offset])
+      const myAppearance = {
+        material: Cesium.Material.fromType('Color', { color })
+      }
+      const myPrimitiveOption = {
+        asynchronous: false,
+        modelMatrix: Cesium.Transforms.eastNorthUpToFixedFrame(position),
+        geometryInstances: [
+          new Cesium.GeometryInstance({ geometry: line, id: `line${id}` }),
+          new Cesium.GeometryInstance({ geometry: arrow, id: `arrow${id}` })
+        ],
+        appearance: new Cesium.MaterialAppearance(myAppearance)
+      }
+      const myPrimitive = new Cesium.Primitive(myPrimitiveOption)
+      return myPrimitive
+    },
+    translateArrowPosition(geometry, offset) {
+      if(!Array.isArray(offset)) return
+      const scratchOffset = new Cesium.Cartesian3()
+      scratchOffset.x = offset[0] //0
+      scratchOffset.y = offset[1] //0
+      scratchOffset.z = offset[2] //長度除以2
+      let geometryPositionLength = geometry.attributes.position.values.length
+      for (let i = 0; i < geometryPositionLength; i += 3) {
+        geometry.attributes.position.values[i] += scratchOffset.x //0
+        geometry.attributes.position.values[i + 1] += scratchOffset.y //0
+        geometry.attributes.position.values[i + 2] += scratchOffset.z //長度除以2
+      }
+    },
+    rotatePrimitive(type, primitive) {
+      let myFunction = type == 'x' ? 'fromRotationX' : 'fromRotationY'
+      const degree = Cesium.Matrix3[myFunction](Cesium.Math.toRadians(90));
+      const rotation = Cesium.Matrix4.fromRotationTranslation(degree);
+      Cesium.Matrix4.multiply(
+          primitive.geometryInstances[0].modelMatrix,
+          rotation,
+          primitive.geometryInstances[0].modelMatrix
+      );
+      Cesium.Matrix4.multiply(
+          primitive.geometryInstances[1].modelMatrix,
+          rotation,
+          primitive.geometryInstances[1].modelMatrix
+      );
+      return primitive
     },
     flyToTaiwan() {
       let { latitude, longitude } = this.taiwanPosition
